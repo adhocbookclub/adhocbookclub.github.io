970994771e4cc78602fc8ef0dc5f869666b81fa7
Jenny Shi <jennpeare@gmail.com>
2015-07-26 01:10:20 -0400

    summer 2015 nomination list

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 // activate tooltips for members page
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip({
+  $('#members [data-toggle="tooltip"]').tooltip({
     placement: "bottom",
     html: true,
     trigger: "click hover"
   });
 });
+
+$('#nomination').on('show.bs.modal', function (event) {
+  var cover = $(event.relatedTarget); // Button that triggered the modal
+  var title = cover.data('title');
+  var author = cover.data('author');
+  var summary = cover.data('summary');
+  var goodreads = cover.data('goodreads');
+  var amazon = cover.data('amazon');
+
+  var modal = $(this);
+  modal.find('.modal-title').html(title + ' <small>' + author + '</small>');
+  modal.find('.modal-body').html(summary);
+  modal.find('.modal-footer').html('<a href="http://bit.ly/' + goodreads + '" target="_blank">Goodreads</a> &middot; <a href="http://amzn.to/' + amazon + '" target="_blank">Amazon</a>');
+})
